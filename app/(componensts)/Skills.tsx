6fec4fe2ff83a5f9da4bc019c2c093f79e7ac267
EzequielPires <ezequiel.pires082000@gmail.com>
2023-09-19 17:55:50 -0300

    Commit

--- app/(componensts)/Skills.tsx
+++ app/(componensts)/Skills.tsx
@@ -7,7 +7,7 @@ export function Skills() {
         <div className="w-full max-w-7xl mx-auto px-3 xl:px-0 py-20 flex flex-col items-center lg:items-start">
             <span className="text-primary text-lg">../competências</span>
             <h2 className="text-white text-3xl lg:text-4xl font-medium mt-2">Conhecimentos</h2>
-            <div className="grid grid-cols-2 lg:grid-cols-4 mt-12 gap-2">
+            <div className="w-full grid grid-cols-2 lg:grid-cols-4 mt-12 gap-2">
                 {skills.map(skill => <CardSkill skill={skill}/>)}
             </div>
         </div>
